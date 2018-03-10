82d777a0266ed541cf8b3a581586aab516f3d533
woobinyim <woobin.kris.yim@gmail.com>
2018-03-10 16:03:45 +0900

    ㅛ

---
+++ tw1.py
@@ -0,0 +1,6 @@
+input_a=float(input("첫번째 숫자>"))
+input_b=float(input("두번째 숫자>"))
+print("덧셈결과:",input_a+input_b)
+print("뻴셈결과:",input_a-input_b)
+print("곱셈결과:",input_a*input_b)
+print("나눗셈결과:",input_a/input_b)
