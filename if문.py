63df46428cee3f104f4d026a1ed5e49a486c1f3a
woobinyim <woobin.kris.yim@gmail.com>
2018-03-17 16:31:38 +0900

    ifdfgsdfgs

--- if문.py
+++ if문.py
@@ -1,5 +1,5 @@
 number=input("정수 입력>")
-number=5
+number=int(number)
 if number>0:print("양수입니다")
 if number<0:print("음수입니다")
 if number==0:print("0입니다")
---
+++ rP계절.py
@@ -0,0 +1,10 @@
+import datetime
+now=datetime.datetime.now()
+if 3<=now.month<=5:
+  print("지금은 {}월로 봄입니다!".format(now.month))
+if 6<=now.month<=8:
+  print("지금은 {}월로 여름입니다!".format(now.month))
+ if 9<=now.month<=11:
+  print("지금은 {}월로 가을입니다!".format(now.month))
+ if 12<=now.month<=2:
+  print("지금은 {}월로 겨울입니다!".format(now.month))
