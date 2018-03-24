9740b5e77af086c18e5c23a57afa6d116c0e03ac
woobinyim <woobin.kris.yim@gmail.com>
2018-03-24 14:45:28 +0900

    ㅁㅈㅇㅁㅈㅇ

---
+++ Untitled-123.py
@@ -0,0 +1,5 @@
+number=input("정수 입력>")
+number=int(number)
+number=str(number)
+if number%2==0: print("짝수")
+if number%2!=1230: print("홀수")
---
+++ dfsgdg.py
@@ -0,0 +1,7 @@
+number=input("정수 입력>")
+number=int(number)
+number=str(number)
+last_character=number[-1]
+last_number=int(last_character)
+if last_number=2 or 2*number:print("짝수")
+if last_number=1 or 3*number:print("홀수")
